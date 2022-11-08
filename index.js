b34a8452251486d649cd8fb913f28dce0eb876af
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-11-08 17:50:10 +0600

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+// const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -11,8 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// OekyZ3SGth6wmlRW
-// geniusUser
 
 // data create/lode mongodB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6oc0gv0.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,33 +21,65 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service')
+        const orderCollection = client.db('geniusCar').collection('order');
+        //AUTH
+        // app.post('/login', async (req, res) => {
+        //     const user = req.body;
+        //     const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        //         expiresIn:"id"
+            
+        //     });
+        //     res.send({accessToken});
+        // })
+
+        //SERVICES API 
+
+
+
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
-        res.send(services);
+            res.send(services);
         });
- // data gate 
-app.get('/service/:id',async(req,res) =>{
-    const id = req.params.id;
-    const query={_id:ObjectId(id)};
-    const service =await serviceCollection.findOne(query);
-    res.send(service)
-})
-// data post
-app.post('/service',async(req,res) =>{
-    const newService =req.body;
-    const result = await serviceCollection.insertOne(newService);
-    res.send(result);
-})
+        // data gate 
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+        // data post
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        })
 
-//Delete
-app.delete('/service/:id',async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)};
-    const result = await serviceCollection.deleteOne(query);
-    res.send(result)
-});
+        //Delete
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result)
+        });
+
+        // order collection Api
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+
     }
     finally {
 
@@ -63,6 +94,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Listing to port');
 })
-
-// projectUser
-// 7Nt4wktCSg5p1U9V
